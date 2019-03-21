35107ac3fd5da8e93590446b6644fc2b01a9f850
Kyle Babcock <kylerbabcock@gmail.com>
2019-03-21 17:58:45 -0400

    concert url works but returns no usable information

--- liri.js
+++ liri.js
@@ -1,29 +1,39 @@
 require("dotenv").config();
+var axios = require("axios");
 var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+
 
 var userInput = process.argv;
 var userRequest = userInput[2];
 var userInfo = userInput[3];
 
-for (i = 4; i < userInput; i++)
+for (i = 4; i < userInput.length; i++) {
+    userInfo += "+" + userInput[i];
+}
 
 switch(userRequest) {
     case "concert-this":
     concert();
     break;
 
-    case "spotify-this":
-    spotify();
+    case "spotify-this-song":
+    spotifySearch();
     break;
 
     default:
-    console.log(error);
+    console.log("error");
 }
 
-function concert (x) {
-
+function concert () {
+var queryURL = "https://rest.bandsintown.com/artists/" + userInfo + "/events?app_id=codingbootcamp"
+console.log(queryURL);
+axios.get(queryURL).then(function(response) {
+    console.log(response);
+})
 }
 
-function spotify (x) {
+function spotifySearch () {
 
 }
