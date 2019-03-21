84e7c79f2bbdab79a67839d9ed67c31132795a9a
Kyle Babcock <kylerbabcock@gmail.com>
2019-03-21 19:24:15 -0400

    movie and spotify api is working

--- liri.js
+++ liri.js
@@ -22,18 +22,41 @@ switch(userRequest) {
     spotifySearch();
     break;
 
+    case "movie-this":
+    movieSearch();
+    break;
+
     default:
     console.log("error");
 }
 
 function concert () {
-var queryURL = "https://rest.bandsintown.com/artists/" + userInfo + "/events?app_id=codingbootcamp"
+var queryURL = "https://rest.bandsintown.com/artists/" + userInfo + "/events?app_id=codingbootcamp";
 console.log(queryURL);
 axios.get(queryURL).then(function(response) {
     console.log(response);
-})
+});
 }
 
 function spotifySearch () {
+    spotify.search({ type: "track", query: userInfo, limit: 1}).then(function(response) {
+        console.log("Artists: " + response.tracks.items[0].album.artists[0].name);
+        console.log("Song Name: " + response.tracks.items[0].name);
+        console.log("Song Preview: " + response.tracks.items[0].preview_url);
+        console.log("Album Name: " + response.tracks.items[0].album.name);
 
+    })
+}
+
+function movieSearch () {
+    var queryURL = "https://www.omdbapi.com/?t=" + userInfo + "&y=&plot=short&apikey=65526309";
+    axios.get(queryURL).then(function(response) {
+        console.log("Title: " + response.data.Title);
+        console.log("Release Year: " + response.data.Year);
+        console.log("IMDB Rating: " + response.data.imdbRating);
+        console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+        console.log("Country Produced in: " + response.data.Country);
+        console.log("Plot: " + response.data.Plot);
+        console.log("Actors: " + response.data.Actors);
+    })
 }
