9c95823170b586f1a9273f751d3376cc98827ef9
Kyle Babcock <kylerbabcock@gmail.com>
2019-03-23 14:44:24 -0400

    added the if no user info entered for movie that it assumes the value of mr. nobody

--- liri.js
+++ liri.js
@@ -15,58 +15,60 @@ for (i = 4; i < userInput.length; i++) {
     userInfo += "+" + userInput[i];
 }
 
-function runInfo () {
+function runInfo() {
 
-switch(userRequest) {
-    case "concert-this":
-    concert();
-    break;
+    switch (userRequest) {
+        case "concert-this":
+            concert();
+            break;
 
-    case "spotify-this-song":
-    spotifySearch();
-    break;
+        case "spotify-this-song":
+            spotifySearch();
+            break;
 
-    case "movie-this":
-    movieSearch();
-    break;
+        case "movie-this":
+            movieSearch();
+            break;
 
-    case "do-what-it-says":
-    checkFile();
-    break;
+        case "do-what-it-says":
+            checkFile();
+            break;
 
-    default:
-    console.log("Invalid Input");
-}
+        default:
+            console.log("Invalid Input");
+    }
 }
 runInfo();
-function concert () {
-var queryURL = "https://rest.bandsintown.com/artists/" + userInfo + "/events?app_id=codingbootcamp";
-console.log(queryURL);
-axios.get(queryURL).then(function(response) {
-    for (i = 0; i < response.data.length; i++) {
-        console.log("Venue Name: " + response.data[i].venue.name);
-        console.log("Venue Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region);
-        console.log("Date of event: " + moment(response.data[i].datetime).format("L"));
-    }
-});
+function concert() {
+    var queryURL = "https://rest.bandsintown.com/artists/" + userInfo + "/events?app_id=codingbootcamp";
+    axios.get(queryURL).then(function (response) {
+        for (i = 0; i < response.data.length; i++) {
+            console.log("Venue Name: " + response.data[i].venue.name);
+            console.log("Venue Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region);
+            console.log("Date of event: " + moment(response.data[i].datetime).format("L"));
+        }
+    });
 }
 
-function spotifySearch () {
+function spotifySearch() {
     if (userInfo === undefined) {
         userInfo = "The+Sign+Ace+of+Base";
     }
-    spotify.search({ type: "track", query: userInfo, limit: 1}).then(function(response) {
+    spotify.search({ type: "track", query: userInfo, limit: 1 }).then(function (response) {
         console.log("Artists: " + response.tracks.items[0].album.artists[0].name);
         console.log("Song Name: " + response.tracks.items[0].name);
         console.log("Song Preview: " + response.tracks.items[0].preview_url);
         console.log("Album Name: " + response.tracks.items[0].album.name);
-    
+
     })
 }
 
-function movieSearch () {
+function movieSearch() {
+    if (userInfo === undefined) {
+        userInfo = "Mr.+Nobody";
+    }
     var queryURL = "https://www.omdbapi.com/?t=" + userInfo + "&y=&plot=short&apikey=65526309";
-    axios.get(queryURL).then(function(response) {
+    axios.get(queryURL).then(function (response) {
         console.log("Title: " + response.data.Title);
         console.log("Release Year: " + response.data.Year);
         console.log("IMDB Rating: " + response.data.imdbRating);
@@ -77,11 +79,11 @@ function movieSearch () {
     })
 }
 
-function checkFile () {
-    fs.readFile("random.txt", "utf8", function(error, data) {
+function checkFile() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
         if (error) {
             console.log(error);
-          }
+        }
         var dataArr = data.split(",");
         // console.log(dataArr);
         var dataArr2 = dataArr[1].split(" ");
@@ -94,6 +96,6 @@ function checkFile () {
         console.log(userRequest);
         console.log(userInfo);
         runInfo();
-        })
-        
+    })
+
 }
