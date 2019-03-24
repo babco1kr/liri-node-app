3059d6f8d9261bf37a211a2ed339e1aa4b8cd1dc
Kyle Babcock <kylerbabcock@gmail.com>
2019-03-24 12:21:27 -0400

    added appending to the log file

--- liri.js
+++ liri.js
@@ -6,7 +6,6 @@ var spotify = new Spotify(keys.spotify);
 var moment = require("moment");
 var fs = require("fs");
 
-
 var userInput = process.argv;
 var userRequest = userInput[2];
 var userInfo = userInput[3];
@@ -43,62 +42,105 @@ function concert() {
     var queryURL = "https://rest.bandsintown.com/artists/" + userInfo + "/events?app_id=codingbootcamp";
     axios.get(queryURL).then(function (response) {
         for (i = 0; i < response.data.length; i++) {
-            console.log("Venue Name: " + response.data[i].venue.name);
-            console.log("Venue Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region);
-            console.log("Date of event: " + moment(response.data[i].datetime).format("L"));
+            var name = "Venue Name: " + response.data[i].venue.name;
+            var location = "Venue Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region;
+            var date = "Date of event: " + moment(response.data[i].datetime).format("L");
+            console.log(name);
+            console.log(location);
+            console.log(date);
+
+            fs.appendFile("log.txt", name + "\n" + location + "\n" + date + "\n" + "" + "\n", function(error) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log("Data added to the log");
+                }
+            })
         }
     });
 }
 
+// Function called when loking for song information
 function spotifySearch() {
     if (userInfo === undefined) {
         userInfo = "The+Sign+Ace+of+Base";
     }
     spotify.search({ type: "track", query: userInfo, limit: 1 }).then(function (response) {
-        var artists = response.tracks.items[0].album.artists[0].name;
+        var artists = "Artists: " + response.tracks.items[0].album.artists[0].name;
+        // A for loop in case there is more than one artist in a song
         for (i = 1; i < response.tracks.items[0].album.artists.length; i++) {
             artists += ", " + response.tracks.items[0].album.artists[i].name;
         }
-        console.log("Artists: " + artists);
-        console.log("Song Name: " + response.tracks.items[0].name);
-        console.log("Song Preview: " + response.tracks.items[0].preview_url);
-        console.log("Album Name: " + response.tracks.items[0].album.name);
-
+        // Assigns variables for use in appending to the log file
+        var name = "Song Name: " + response.tracks.items[0].name;
+        var preview = "Song Preview: " + response.tracks.items[0].preview_url;
+        var album = "Album Name: " + response.tracks.items[0].album.name;
+        // Console logs needed information
+        console.log(artists);
+        console.log(name);
+        console.log(preview);
+        console.log(album);
+        //Appending to the log.txt file
+        fs.appendFile("log.txt", artists + "\n" + name + "\n" + preview + "\n" + album + "\n" + "" + "\n", function (error) {
+            // Checks to see if there is an error appending to the file
+            if (error) {
+                console.log(error);
+            } else {
+                console.log("Data added to the log");
+            }
+        })
     })
 }
 
+// Function called when looking for movie information
 function movieSearch() {
     if (userInfo === undefined) {
         userInfo = "Mr.+Nobody";
     }
     var queryURL = "https://www.omdbapi.com/?t=" + userInfo + "&y=&plot=short&apikey=65526309";
     axios.get(queryURL).then(function (response) {
-        console.log("Title: " + response.data.Title);
-        console.log("Release Year: " + response.data.Year);
-        console.log("IMDB Rating: " + response.data.imdbRating);
-        console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
-        console.log("Country Produced in: " + response.data.Country);
-        console.log("Plot: " + response.data.Plot);
-        console.log("Actors: " + response.data.Actors);
+        var title = "Title: " + response.data.Title;
+        var year = "Release Year: " + response.data.Year;
+        var IMDBRating = "IMDB Rating: " + response.data.imdbRating;
+        var RTRating = "Rotten Tomatoes Rating: " + response.data.Ratings[1].Value;
+        var country = "Country Produced in: " + response.data.Country;
+        var plot = "Plot: " + response.data.Plot;
+        var actors = "Actors: " + response.data.Actors;
+        // Console logs the needed information
+        console.log(title);
+        console.log(year);
+        console.log(IMDBRating);
+        console.log(RTRating);
+        console.log(country);
+        console.log(plot);
+        console.log(actors);
+
+        fs.appendFile("log.txt", title + "\n" + year + "\n" + IMDBRating + "\n" + RTRating + "\n" + country + "\n" + plot + "\n" + actors + "\n" + "" + "\n" , function(error) {
+            // Checkes to see if theres an error appending to the file
+            if (error) {
+                console.log(error);
+            } else {
+                console.log("Data Added to Log");
+            }
+        })
     })
 }
 
+// Function for reading information from the file
 function checkFile() {
     fs.readFile("random.txt", "utf8", function (error, data) {
         if (error) {
             console.log(error);
         }
+        // Reassigning the values of the variables to the values found in the file
         var dataArr = data.split(",");
-        // console.log(dataArr);
         var dataArr2 = dataArr[1].split(" ");
-        // console.log(dataArr2);
         userRequest = dataArr[0];
-        userInfo = dataArr2[0]
+        userInfo = dataArr2[0];
         for (i = 1; i < dataArr2.length; i++) {
             userInfo += "+" + dataArr2[i];
         }
-        console.log(userRequest);
-        console.log(userInfo);
+        // Calling the function to run the switch again with new values
         runInfo();
     })
 
